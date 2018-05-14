fcd876b28ad9480794cadac4a03baf0bfaed2e24
mzaini30 <muhzaini30@gmail.com>
2018-05-14 10:59:13 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -1,10 +1,7 @@
 $(function(){
-	animasi = function(elemen, efek){
-		$(elemen).addClass("hidden").viewportChecker({
-			classToAdd: function(){
-				return 'visible animated '+efek
-			}
-		})	
-	}
-	animasi('.muncul', 'rubberBand')
+	$('.muncul').addClass("hidden").viewportChecker({
+		classToAdd: function(){
+			return 'visible animated rubberBand'
+		}
+	})
 })
